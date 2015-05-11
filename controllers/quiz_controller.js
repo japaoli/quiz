6912fde398b546609786c2c887e86a5758d08373
japaoli <jesus.aparicio.oliver@alumnos.upm.es>
2015-05-11 11:54:29 +0200

    Editar pregunta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -24,46 +24,46 @@ exports.load = function(req,res,next,quizId){
   ).catch(function(error){next(error)});
 };
 
-// GET /quizes/new
-exports.new = function(req, res) {
-  var quiz = models.Quiz.build(// crea objeto quiz 
-    {pregunta: "Pregunta", respuesta: "Respuesta"}
-  );
 
-  res.render('quizes/new', {quiz: quiz, errors: []});
-};
 
-// POST /quizes/create
-exports.create = function(req, res) {
-  var quiz = models.Quiz.build( req.body.quiz );
 
-quiz
-  .validate()
-  .then(
-    function(err){
-      if (err) {
-        res.render('quizes/new', {quiz: quiz, errors: err.errors});
-      } else {
-        quiz // save: guarda en DB campos pregunta y respuesta de quiz
-        .save({fields: ["pregunta", "respuesta"]})
-        }      // res.redirect: Redirección HTTP a lista de preguntas
-    }
+//GET /quizes
+exports.index = function(req,res){
+	var search = req.query.search;
+	if (search !== undefined){
+		search = search.replace(" ","%");
+		search = "%"+search+"%";
+	}
+	models.Quiz.findAll({where: ["pregunta like ?", search]}).then(function(quizes){
+		res.render('quizes/index.ejs',{quizes:quizes, errors: []});
+	}
   ).catch(function(error){next(error)});
 };
 
 
+// PUT /quizes/:id
+exports.update = function(req, res) {
+  req.quiz.pregunta  = req.body.quiz.pregunta;
+  req.quiz.respuesta = req.body.quiz.respuesta;
 
-
-
-
-
-
-//GET /quizes/:id
-exports.show = function(req,res){
-	models.Quiz.find(req.params.quizId).then(function(quiz){
-		res.render('quizes/show',{quiz:quiz, errors: []});
-	})
+  req.quiz
+  .validate()
+  .then(
+    function(err){
+      if (err) {
+        res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+      } else {
+        req.quiz     // save: guarda campos pregunta y respuesta en DB
+        .save( {fields: ["pregunta", "respuesta"]})
+        .then( function(){ res.redirect('/quizes');});
+      }     // Redirección HTTP a lista de preguntas (URL relativo)
+    }
+  );
 };
+
+
+
+
 
 //GET /quizes/:id/answer
 exports.answer = function(req, res) {
@@ -80,16 +80,53 @@ exports.answer = function(req, res) {
   );
 };
 
-//GET /quizes
-exports.index = function(req,res){
-	var search = req.query.search;
-	if (search !== undefined){
-		search = search.replace(" ","%");
-		search = "%"+search+"%";
-	}
-	models.Quiz.findAll({where: ["pregunta like ?", search]}).then(function(quizes){
-		res.render('quizes/index.ejs',{quizes:quizes, errors: []});
-	}
-  ).catch(function(error){next(error)});
+
+// GET /quizes/new
+exports.new = function(req, res) {
+  var quiz = models.Quiz.build(// crea objeto quiz 
+    {pregunta: "Pregunta", respuesta: "Respuesta"}
+  );
+
+  res.render('quizes/new', {quiz: quiz, errors: []});
 };
+
+// POST /quizes/create
+exports.create = function(req, res) {
+  var quiz = models.Quiz.build( req.body.quiz );
+
+  quiz
+  .validate()
+  .then(
+    function(err){
+      if (err) {
+        res.render('quizes/new', {quiz: quiz, errors: err.errors});
+      } else {
+        quiz // save: guarda en DB campos pregunta y respuesta de quiz
+        .save({fields: ["pregunta", "respuesta"]})
+        .then( function(){ res.redirect('/quizes')}) 
+      }      // res.redirect: Redirección HTTP a lista de preguntas
+    }
+  );
+};
+
+// GET /quizes/:id/edit
+exports.edit = function(req, res) {
+  var quiz = req.quiz;  // req.quiz: autoload de instancia de quiz
+
+  res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
+
+
+// GET /quizes/:id
+exports.show = function(req, res) {
+  res.render('quizes/show', { quiz: req.quiz, errors: []});
+};            // req.quiz: instancia de quiz cargada con autoload
+
+
+
+
+
+
+
 
