ab2013dc87ab9472d51921e6cee8a7c171d9bdb1
japaoli <jesus.aparicio.oliver@alumnos.upm.es>
2015-05-16 02:38:38 +0200

    Auto-logout

--- app.js
+++ app.js
@@ -51,6 +51,25 @@ app.use(function(req,res,next){
 
 
 
+//Logout automático
+app.use(function(req,res,next){
+    if (req.session.user){
+        if(req.session.user.inicio){
+            console.log("user: "+req.session.user.inicio+" \n");
+            console.log(new Date().getTime()+" \n");
+            if((new Date().getTime()-req.session.user.inicio)>120000){
+                req.session.user = undefined;
+               
+            }else {req.session.user.inicio = new Date().getTime();}
+           
+        } else {req.session.user.inicio = new Date().getTime();}
+
+    }
+     next();
+ });
+
+
+
 app.use('/author',author);
 app.use('/', routes);
 
